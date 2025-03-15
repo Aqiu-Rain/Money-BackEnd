ee4aeb26d9148902206e0c09b9886da0110f95f8
rain <825038824@qq.com>
2025-03-15 12:27:58 +0800

    fix: 解决搜索结果无法导出的问题

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ t = threading.Thread(target=uvicorn.run, args=(app,), kwargs={"host": "127.0.0.1
 t.daemon = True
 t.start()
 
+webview.settings['ALLOW_DOWNLOADS'] = True
 # 在PyWebview应用程序中加载FastAPI应用程序的URL
 webview.create_window('Cash Management SW_GRACETEK', 'http://127.0.0.1:8000', maximized=True, min_size=(1400, 800))
 webview.start()
